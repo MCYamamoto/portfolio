76cdf96ca3f8dbc9a3aa1f8ae714d5de84719d00
MCYamamoto <yamamoto@moltocarina.co.jp>
2020-09-07 01:57:27 +0900

    トップページのヘッダー作成中

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
 # portfolio
 モルトカリーナ山本ポートフォリオ
+
+
+
+## 環境構築
+
+
+
+<pre>
+    yarn install
+    yarn build
+</pre>
+
+
+
+### <注意点>
+
+node_module/resolve-url-loader/index.js
+
+<pre>
+      var options = Object.assign(
+    {
+      sourceMap: loader.sourceMap,
+      engine   : 'postcss',
+      silent   : false,
+      absolute : false,
+      keepQuery: false,
+      removeCR : true,  //<--- 元々はfalseだったが、trueにしないとbuildエラーになった
+
+</pre>
+
+
+
+
+
+
+
--- mcyama_portfolio/src/pages/toppage/toppage.tsx
+++ mcyama_portfolio/src/pages/toppage/toppage.tsx
@@ -6,13 +6,27 @@ import {Link} from 'react-router-dom';
 
 //CSS
 import "./toppage.scss";
+import {} from "semantic-ui-react"
 
 const TopPage:FC<{}> = ((props:{}) => {
-    const toppage_image = require('./../../image/toppageimage.jpg');
     return (
         <div>
-            <img className='toppage-image' src={toppage_image} alt='Top Page Image'/>
-            <Link to='/profile'>プロフィール</Link>
+            <header>
+                <div className="header__wrapper">
+                    <ul>
+                        <li className="header__wrapper__ul__li--li1">Engineer</li>
+                        <li className="header__wrapper__ul__li--li2">K.Yamamoto's</li>
+                        <li className="header__wrapper__ul__li--li3">Portfolio</li>
+                    </ul>
+                </div>
+                <nav>
+                    <ul className="header__nav">
+                        <li><Link to='/profile'>PROFILE</Link></li>
+                        <li><Link to='/profile'>SKILL</Link></li>
+                        <li><Link to='/profile'>CONTACT</Link></li>
+                    </ul>
+                </nav>
+            </header>
         </div>
     );
 });
